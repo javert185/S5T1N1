914362e36f4e5d9479872f263f165dec703cf9cb
javert185 <124713607+javert185@users.noreply.github.com>
2023-08-30 16:40:41 +0200

    Delete S05T01N01CarapuigMasnouSergi directory

--- S05T01N01CarapuigMasnouSergi/src/main/java/cat/itacademy/barcelonactiva/carapuigmasnou/sergi/s05/t01/n01/model/repository/SucursalRepository.java
+++
@@ -1,8 +0,0 @@
-package cat.itacademy.barcelonactiva.carapuigmasnou.sergi.s05.t01.n01.model.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import cat.itacademy.barcelonactiva.carapuigmasnou.sergi.s05.t01.n01.model.domain.Sucursal;
-
-public interface SucursalRepository extends JpaRepository<Sucursal, Integer> {
-    
-}
